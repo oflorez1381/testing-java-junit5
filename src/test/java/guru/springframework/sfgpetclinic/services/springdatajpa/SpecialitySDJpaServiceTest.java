245fc3ad147455180ef14a226f902b6c3c349e65
oflorez1381 <oscarflorez1381@gmail.com>
2019-12-17 20:21:23 -0500

    11.2 Throwing Exeptions with Mockito

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -11,9 +11,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -146,6 +146,15 @@ class SpecialitySDJpaServiceTest {
     }
 
     @Test
+    void testDeleteBDDThrow() {
+        willThrow(new RuntimeException("boom")).given(specialtyRepository).delete(any());
+
+        assertThrows(RuntimeException.class ,() -> specialtyRepository.delete(new Speciality()));
+
+        then(specialtyRepository).should().delete(any());
+    }
+
+    @Test
     void testFindById(){
         Speciality speciality = new Speciality();
 
@@ -170,5 +179,23 @@ class SpecialitySDJpaServiceTest {
         assertThat(foundSpeciality).isNotNull();
         then(specialtyRepository).should().findById(1L);
         then(specialtyRepository).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void testDoThrow() {
+        doThrow(new RuntimeException("boom")).when(specialtyRepository).delete(any());
+
+        assertThrows(RuntimeException.class, () -> specialtyRepository.delete(new Speciality()));
+
+        verify(specialtyRepository).delete(any());
+    }
+
+    @Test
+    void testFindByIdThrows() {
+        given(specialtyRepository.findById(1L)).willThrow(new RuntimeException("boom"));
+
+        assertThrows(RuntimeException.class, () -> specialtyRepository.findById(1L));
+
+        then(specialtyRepository).should().findById(1L);
     }
 }
